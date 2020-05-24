f77ef5134b4186ce2d618b9da9851b164ac9bb55
Anton Tkachev <anton.a.tkachev@gmail.com>
2020-05-24 00:53:14 -0700

    Update example.py

--- example.py
+++ example.py
@@ -2,10 +2,10 @@
 
 from MS8607 import MS8607
 
-pht = MS8607()
+sensor = MS8607()
 
-print(pht.get_t())      # get temperature (with two auxilary variables)
-print(pht.get_rh())     # get humidity
-print(pht.get_tp())     # get temperature and pressure
-print(pht.get_th())     # get temperature and humidity
-print(pht.get_tph())    # get temperature, pressure and humidity
+print(sensor.get_t())      # get temperature (with two auxilary variables)
+print(sensor.get_rh())     # get humidity
+print(sensor.get_tp())     # get temperature and pressure
+print(sensor.get_th())     # get temperature and humidity
+print(sensor.get_tph())    # get temperature, pressure and humidity
